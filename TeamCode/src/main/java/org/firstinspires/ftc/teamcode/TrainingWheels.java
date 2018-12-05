0611086d2b68f7fc3bc26e5a7d841610900facf5
DESKTOP-U310M9J\Benla <Benladner2@gmail.com>
2018-12-05 12:04:18 -0800

    added a training wheels file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrainingWheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrainingWheels.java
@@ -1,15 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
  * Created by Benla on 11/24/2018.
  */
 
-public class TrainingWheels extends RobotsBase
+@TeleOp(group = "Sprockets", name = "Training Wheels")
+public class TrainingWheels extends LinearOpMode
 {
-    public void DefineOpMode ()
+    public DcMotor leftDrive;
+    public DcMotor rightDrive;
+
+    public void runOpMode() throws InterruptedException
     {
+        leftDrive = hardwareMap.dcMotor.get("leftDrive");
+        rightDrive = hardwareMap.dcMotor.get("rightDrive");
+
+        rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
         leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
@@ -19,6 +31,8 @@ public class TrainingWheels extends RobotsBase
         {
             leftDrive.setPower(-gamepad1.left_stick_y);
             rightDrive.setPower(-gamepad1.right_stick_y);
+
+            idle();
         }
     }
 }
